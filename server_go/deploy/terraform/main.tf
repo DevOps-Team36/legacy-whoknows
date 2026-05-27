477ce330b1e5c2724471341c1f98fe3912b0fe30
Hannibal-Privat <hannibal-privat@Mac.home>
2026-05-27 16:25:12 +0200

    feat: add fail2ban, nginx-monitoring, ssh-keys roles and prometheus firewall port

--- server_go/deploy/terraform/main.tf
+++ server_go/deploy/terraform/main.tf
@@ -128,6 +128,13 @@ resource "digitalocean_firewall" "monitoring" {
     source_addresses = ["${digitalocean_droplet.whoknows.ipv4_address}/32"]
   }
 
+  # Prometheus UI
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9090"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "all"
